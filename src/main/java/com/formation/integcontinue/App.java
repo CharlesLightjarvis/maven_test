5f19cd3fbb9623bca7f437bc00ff60be004a37b5
CharlesLightjarvis <118352168+CharlesLightjarvis@users.noreply.github.com>
2024-12-10 10:21:33 +0100

    Jenkins Pipeline

--- src/main/java/com/formation/integcontinue/App.java
+++ src/main/java/com/formation/integcontinue/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bonjour Tout Le Monde!" );
+        System.out.println( "Jenkins Pipeline!" );
     }
 }
